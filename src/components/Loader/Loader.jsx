e70662ce5323797c06fd82a921fb682a625a9f2c
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-03-05 23:07:24 +0200

    fix css

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import s from './Loader.module.css';
 export default function Loader() {
   return (
     <div className={s.overlay}>
-      <DotLoader size={250} color={'#461646'} className={s.loader} />
+      <DotLoader size={250} color={'#e5d1fa'} className={s.loader} />
     </div>
   );
 }
